e8a459644e20ac52316a22f08fa74a4400fc293f
Glitch (keen-rare-pluto) <none>
2021-10-21 06:26:28 +0000

    Node basics

--- myApp.js
+++ myApp.js
@@ -1,39 +1,42 @@
 var express = require('express');
 var app = express();
 
+// console.log('Hello World')
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/json', (req, res) => {
+  res.json({
+    message: process.env.MESSAGE_STYLE === 'uppercase' ? 'Hello json'.toUpperCase() : 'Hello json'
+  })
+})
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString()
+  next()
+}, (req, res) => {
+  res.json({
+    time: req.time
+  })
+})
+
+app.get('/:word/echo', (req, res) => {
+  const word = req.params.word
+  res.json({echo: word})
+})
+
+app.get('/name', (req, res) => {
+  res.json({ name: `${req.query.first} ${req.query.last}` })
+})
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
